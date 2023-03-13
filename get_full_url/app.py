7dc48380eedcb442931240de4fad8296bb8652bd
Omer Aplatony <omerap12@gmail.com>
2023-03-13 16:22:10 +0200

    Write url extraction function

--- get_full_url/app.py
+++ get_full_url/app.py
@@ -1,10 +1,29 @@
 import json
+import boto3
+
+dynamodb = boto3.client('dynamodb', endpoint_url='http://localhost:8000') # connect to dynamo db
+
 
 def lambda_handler(event, context):
-    short_url = event['short_url']
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message" : short_url
-        }),
-    }
+    shortened_url = event['shortened_url']
+    response = dynamodb.get_item(
+        TableName='url-table',
+        Key = {
+            'shortened_url' : {'S':shortened_url}
+        }
+    )
+    item = response['Item']['url']["S"]
+    if item:
+        return {
+            "statusCode": 200,
+            "body": json.dumps({
+                "message" : item
+            }),
+        }
+    else:
+        return {
+            "statusCode": 400,
+            "body": json.dumps({
+                "message" : "Not exist"
+            }),
+        }
