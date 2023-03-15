257ee3352492cfd6f72246cf6195b7ea8555a537
Omer Aplatony <omerap12@gmail.com>
2023-03-15 16:35:39 +0200

    managed to pull info from the database

--- get_full_url/app.py
+++ get_full_url/app.py
@@ -1,29 +1,37 @@
 import json
 import boto3
 
-dynamodb = boto3.client('dynamodb', endpoint_url='http://localhost:8000') # connect to dynamo db
-
 
 def lambda_handler(event, context):
-    shortened_url = event['shortened_url']
-    response = dynamodb.get_item(
+    dynamodb_client = boto3.client('dynamodb', endpoint_url='http://abp-sam-nestjs-dynamodb:8000') # connect to dynamo db
+    if dynamodb_client:
+        print("Dynamo-connected.")
+    parameters_sent = json.loads(event['body'])
+    url_requested = parameters_sent['shortened_url']
+
+    # getting the item from the database
+    response = dynamodb_client.get_item(
         TableName='url-table',
         Key = {
-            'shortened_url' : {'S':shortened_url}
+            'shortened_url' : {'S':url_requested}
         }
     )
-    item = response['Item']['url']["S"]
-    if item:
+
+    # check if the item is within the database
+    try:
+        item = response['Item']['url']['S']
+    # id not return 404
+    except Exception as e:
         return {
-            "statusCode": 200,
-            "body": json.dumps({
-                "message" : item
-            }),
-        }
+                "statusCode": 404,
+                "body": json.dumps({
+                    "message" : "Not found"
+                }),
+            }
     else:
         return {
-            "statusCode": 400,
-            "body": json.dumps({
-                "message" : "Not exist"
-            }),
-        }
+                "statusCode": 200,
+                "body": json.dumps({
+                    "message" : response['Item']['url']['S']
+                }),
+            }
