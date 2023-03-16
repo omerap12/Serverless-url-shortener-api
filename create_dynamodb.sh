9bc4dcc9323e8717ef5005d63b7ef2834f0bd747
Omer Aplatony <omerap12@gmail.com>
2023-03-16 14:55:32 +0200

    Set the script outside

---
+++ create_dynamodb.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+function usage {
+    echo "USAGE: $0 PORT_NUMBER"
+    echo "Create and run the local dynamodb at port number."
+}
+
+PORT=$1
+if [ -z $PORT ]; then 
+    usage
+    exit
+fi
+echo "Create dynamodb docker on port $PORT"
+sed -i '' -e "s/8000:8000/$PORT:$PORT/" dynamo-db-configuration/docker-compose.yml
+docker-compose -f dynamo-db-configuration/docker-compose.yml up &
+
+echo "Create user-table"
+aws dynamodb create-table --cli-input-json file://dynamo-db-configuration/create-table.json --endpoint-url http://localhost:"$PORT"
+if [[ $? -ne 0 ]]; then
+    echo "Error creating table, might be because table already exist."
+    echo "Continue"
+fi
+
+echo "Add dummy value to table"
+aws dynamodb put-item --table-name url-table --item '{"shortened_url": {"S": "abc123"}, "url": {"S": "https://example.com"}}' --endpoint-url http://localhost:"$PORT"
+if [[ $? -ne 0 ]]; then
+    echo "Error put item in table"
+    exit
+fi
+
+echo "Get the dummy value from the db"
+aws dynamodb get-item --table-name url-table --key '{"shortened_url": {"S": "abc123"}}' --endpoint-url http://localhost:"$PORT"
+if [[ $? -ne 0 ]]; then
+    echo "Error get item from table table"
+    exit
+fi
+
+sed -i "s/ENDPOINT_URL: http:\/\/abp-sam-nestjs-dynamodb:8000/ENDPOINT_URL: http:\/\/abp-sam-nestjs-dynamodb:$PATH/" template.yaml
+echo "dynamodb is up and running!"
+echo "run sam build && sam local start-api -t template.yaml --docker-network abp-sam-backend"
