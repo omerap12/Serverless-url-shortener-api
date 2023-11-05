77d913b44841a65f6058dd9f7cd6d80e3903660b
Omer <omerap12@gmail.com>
2023-11-05 15:29:02 +0200

    Added output file

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,4 +1,9 @@
 output "dynamodb_table_id" {
   value = module.dynamodb_table.dynamodb_table_id
 }
-
+output "api-url-read" {
+    value = "${module.api_gateway.apigatewayv2_api_api_endpoint}/read"
+}
+output "api-url-write" {
+    value = "${module.api_gateway.apigatewayv2_api_api_endpoint}/write"
+}
