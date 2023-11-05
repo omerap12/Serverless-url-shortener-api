53cea633c4366d7b363373293f55d04004841aea
Omer <omerap12@gmail.com>
2023-11-05 15:24:27 +0200

    Fixed bug in code

--- code/lambda_write.py
+++ code/lambda_write.py
@@ -9,14 +9,32 @@ logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
     try:
-        short_url = event.get('short_url')
-        full_url = event.get('full_url')
-        logger.info(f"Short url: {short_url}")
-        logger.info(f"Full url: {full_url}")
-        
+        body = event['body']
+        logger.info(body)
+        body = json.loads(body)
+        short_url = body.get('short_url')
+        full_url = body.get('full_url')
+
         if not short_url or not full_url:
             raise ValueError("short_url and full_url are required.")
-                
+        
+        table_name = os.environ.get('TABLE_NAME')
+        if not table_name:
+            raise EnvironmentError("TABLE_NAME environment variable is not set.")
+        
+        dynamodb_client.put_item(
+            TableName=table_name,
+            Item={'shortened_url': {'S': short_url}, 'url': {'S': full_url}}
+        )
+        
+        logger.info("Record added successfully.")
+        return {
+            "statusCode": 200,
+            "body": json.dumps({
+                "message": "Record added"
+            })
+        }
+    
     except ValueError as e:
         logger.error(f"Invalid input: {e}")
         return {
@@ -26,29 +44,11 @@ def lambda_handler(event, context):
             })
         }
     
-    try:
-        table_name = os.environ.get('TABLE_NAME')
-        if not table_name:
-            raise EnvironmentError("TABLE_NAME environment variable is not set.")
-        
-        dynamodb_client.put_item(
-            TableName=table_name,
-            Item={'shortened_url': {'S': short_url}, 'url': {'S': full_url}}
-        )
-    
     except Exception as e:
-        logger.error(f"DB Error: {e}")
+        logger.error(f"Error: {e}")
         return {
             "statusCode": 500,
             "body": json.dumps({
-                "message": "Internal Server Error"
+                "message": str(e)
             })
         }
-    
-    logger.info("Record added successfully.")
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "Record added"
-        })
-    }
