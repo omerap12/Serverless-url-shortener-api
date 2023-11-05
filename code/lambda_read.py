ce2593d8c211d88ed171bc6feb42112bc028f415
Omer <omerap12@gmail.com>
2023-11-05 15:40:50 +0200

    Added comments

--- code/lambda_read.py
+++ code/lambda_read.py
@@ -3,22 +3,31 @@ import boto3
 import os
 import logging
 
+# Initialize DynamoDB client and logger
 dynamodb_client = boto3.client('dynamodb')
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
     try:
+        # Extract the request body from the event
         body = event['body']
+        
+        # Log the received request body
         logger.info(body)
+        
+        # Parse the JSON body into a dictionary
         body = json.loads(body)
 
+        # Retrieve the 'short_url' from the parsed JSON body
         short_url = body.get('short_url')
         
+        # Check if 'short_url' is missing in the request
         if not short_url:
             raise ValueError("short_url is required.")
                 
     except ValueError as e:
+        # Handle invalid input error
         logger.error(f"Invalid input: {e}")
         return {
             "statusCode": 400,
@@ -28,11 +37,17 @@ def lambda_handler(event, context):
         }
     
     try:
+        # Retrieve the DynamoDB table name from environment variables
         table_name = os.environ.get('TABLE_NAME')
+        
+        # Check if TABLE_NAME environment variable is not set
         if not table_name:
             raise EnvironmentError("TABLE_NAME environment variable is not set.")
         
-        response = dynamodb_client.get_item(TableName= table_name, Key = {'shortened_url' : {'S': short_url}})
+        # Query DynamoDB for the item with the provided 'short_url'
+        response = dynamodb_client.get_item(TableName=table_name, Key={'shortened_url': {'S': short_url}})
+        
+        # Check if the item was not found in DynamoDB
         if 'Item' not in response:
             return {
                 "statusCode": 404,
@@ -40,10 +55,13 @@ def lambda_handler(event, context):
                     "message": "Short URL not found"
                 })
             }
+        
+        # Extract the 'full_url' from the DynamoDB response
         item = response['Item']
         full_url = item['url']['S']
     
     except Exception as e:
+        # Handle database-related errors
         logger.error(f"DB Error: {e}")
         return {
             "statusCode": 500,
@@ -52,6 +70,7 @@ def lambda_handler(event, context):
             })
         }
 
+    # Return the 'full_url' in the response
     return {
         "statusCode": 200,
         "body": json.dumps({
--- code/lambda_write.py
+++ code/lambda_write.py
@@ -3,30 +3,44 @@ import boto3
 import os
 import logging
 
+# Initialize DynamoDB client and logger
 dynamodb_client = boto3.client('dynamodb')
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
     try:
+        # Extract the request body from the event
         body = event['body']
+        
+        # Log the received request body
         logger.info(body)
+        
+        # Parse the JSON body into a dictionary
         body = json.loads(body)
+        
+        # Retrieve 'short_url' and 'full_url' from the parsed JSON body
         short_url = body.get('short_url')
         full_url = body.get('full_url')
 
+        # Check if 'short_url' or 'full_url' is missing in the request
         if not short_url or not full_url:
             raise ValueError("short_url and full_url are required.")
         
+        # Retrieve the DynamoDB table name from environment variables
         table_name = os.environ.get('TABLE_NAME')
+        
+        # Check if TABLE_NAME environment variable is not set
         if not table_name:
             raise EnvironmentError("TABLE_NAME environment variable is not set.")
         
+        # Put item into DynamoDB table with 'short_url' and 'full_url' attributes
         dynamodb_client.put_item(
             TableName=table_name,
             Item={'shortened_url': {'S': short_url}, 'url': {'S': full_url}}
         )
         
+        # Log success message
         logger.info("Record added successfully.")
         return {
             "statusCode": 200,
@@ -36,6 +50,7 @@ def lambda_handler(event, context):
         }
     
     except ValueError as e:
+        # Handle invalid input error
         logger.error(f"Invalid input: {e}")
         return {
             "statusCode": 400,
@@ -45,6 +60,7 @@ def lambda_handler(event, context):
         }
     
     except Exception as e:
+        # Handle other errors
         logger.error(f"Error: {e}")
         return {
             "statusCode": 500,
