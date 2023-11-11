616a84072684c1dc7e0f55a8d790b93a852afdd9
Omer <omerap12@gmail.com>
2023-11-11 14:07:14 +0200

    Change format for output

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,9 +1,9 @@
 output "dynamodb_table_name" {
   value = module.dynamodb_table.dynamodb_table_id
 }
-output "api-url-read" {
+output "api_url_read" {
     value = "${module.api_gateway.apigatewayv2_api_api_endpoint}/read"
 }
-output "api-url-write" {
+output "api_url_write" {
     value = "${module.api_gateway.apigatewayv2_api_api_endpoint}/write"
 }
